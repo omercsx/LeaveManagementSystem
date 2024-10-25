0345afd50802f94675b56959ad7e93e3100c6e8a
omercsx <omercagris@gmail.com>
2024-10-25 15:25:46 -0400

    init

---
+++ LeaveManagementSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LeaveManagementSystem.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
